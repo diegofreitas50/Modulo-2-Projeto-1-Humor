e1c483eafa3a3d373522acb346a8936d067f1d52
diegofreitas50 <diegofreitas50@hotmail.com>
2022-03-18 02:09:17 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
 const btn = document.querySelector("#btn");
 
-const images = ["./assets/media/fofo.png", "./assets/media/brincando.png", "./assets/media/dormindo.png"];
+const images = [
+    "./assets/media/lindo.png", 
+    "./assets/media/brincando.png", 
+    "./assets/media/dormindo.png",
+    "./assets/media/comendo.png",
+    "./assets/media/cheio.png",
+    "./assets/media/maloqueiro.png",
+    "./assets/media/bebado.png",
+    "./assets/media/obsceno.png"
+];
 
-const situacao = ["Situação: Fofo", "Situação: Brincando", "Situação: Dormindo"];
+const situacao = [
+    "Situação: Sendo lindo", 
+    "Situação: Brincando", 
+    "Situação: Dormindo",
+    "Situação: Comendo",
+    "Situação: Bucho cheio",
+    "Situação: Maloqueiro",
+    "Situação: Bêbado",
+    "Situação: +18"
+];
 
-const nome = ["Nome: Gatinho", "Nome: Gato selvagem", "Nome: Belo adormecido"];
+const nome = [
+    "Nome: Gatito", 
+    "Nome: Doido das caixas", 
+    "Nome: Gatito tatuzinho",
+    "Nome: Gatito guloso",
+    "Nome: Barrigudo",
+    "Nome: Das quebradas",
+    "Nome: O pinguço",
+    "Nome: Gatito Sedução",
+];
 
 var index = 0;
 
 btn.addEventListener("click", function () {
-    if(index < 2){
+    if(index < (nome.length - 1)){
         index++;
         document.getElementById("gato").src = images[index];
         document.getElementById("situacao").innerText = situacao[index];
